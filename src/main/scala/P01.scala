526dac6739d3fda16677c3eed2f7c49f1911d857
damian.albrun <damian.albrun@globallogic.com>
2015-01-16 17:30:08 -0300

    Problem 1 solution with option result

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -2,6 +2,12 @@ package org.p99.scala
 
 object P01 {
 
-  // TODO
+  def last(numbers: List[Int]): Option[Int] = numbers match {
+    case Nil => None
+    case list => list.tail match {
+      case Nil => Option(list.head)
+      case tail => last(tail)
+    }
+  }
 
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -4,7 +4,15 @@ import org.scalatest._
 
 class P01Spec extends UnitSpec {
 
-  // TODO
+  "given a list with items" should
+    "returns last item" in {
+      val list = List(1, 1, 2, 3, 5, 8)
+      assert(P01.last(list).get == 8)
+  }
+
+  "given an empty list" should "return a None" in {
+    assert(P01.last(List()) == None)
+  }
 
 }
 
