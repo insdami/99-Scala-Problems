bf8f77714224bebf17b3987f514bfe0aeb9700ba
damian.albrun <damian.albrun@globallogic.com>
2015-01-19 15:46:09 -0300

    Problem 4 solution: return list's length

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -1,7 +1,11 @@
 package org.p99.scala
 
 object P04 {
+  def length(list: List[Any]): Any = count(0, list)
 
-  // TODO
+  private def count(fromPosition: Int = 0, list: List[Any]): Int = list match {
+    case Nil | null  => fromPosition
+    case head :: tail => count(fromPosition + 1, tail)
+  }
 
 }
--- src/test/scala/P04Spec.scala
+++ src/test/scala/P04Spec.scala
@@ -4,7 +4,21 @@ import org.scalatest._
 
 class P04Spec extends UnitSpec {
 
-  // TODO
+  "given a list with 3 elements" should "return list's size" in {
+    assert(P04.length(List(1,2,3)) == 3)
+  }
+
+  "given an empty list" should "return 0 as list's size" in {
+    assert(P04.length(List()) == 0)
+  }
+
+  "given a null as parameter" should "return 0" in {
+    assert(P04.length(null) == 0)
+  }
+
+
+
+
 
 }
 
