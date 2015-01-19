ccf1c266bb488c975b63c244273393aaea6f571c
damian.albrun <damian.albrun@globallogic.com>
2015-01-19 17:51:08 -0300

    Solution problem 5: list reverse order

--- src/test/scala/P05Spec.scala
+++ src/test/scala/P05Spec.scala
@@ -4,7 +4,9 @@ import org.scalatest._
 
 class P05Spec extends UnitSpec {
 
-  // TODO
+  "given a list of elements" should "return the same elements but with reverse order" in {
+    assert(P05.reverse(List(1,2,3)) == List(3,2,1))
+  }
 
 }
 
