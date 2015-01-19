f329c6997c60b09104759642b78f52af8ac2a5ed
damian.albrun <damian.albrun@globallogic.com>
2015-01-19 12:11:48 -0300

    Solution problem 2: find penultimate item

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -2,6 +2,10 @@ package org.p99.scala
 
 object P02 {
 
-  // TODO
+  def penultimate(numbers: List[Any]): Any = numbers match {
+    case head :: tail :: Nil => head
+    case head :: tail => penultimate(tail)
+    case _ => Nil
+  }
 
 }
--- src/test/scala/P02Spec.scala
+++ src/test/scala/P02Spec.scala
@@ -4,7 +4,24 @@ import org.scalatest._
 
 class P02Spec extends UnitSpec {
 
-  // TODO
+  "given a list of numbers" should "return the last but one" in {
+    assert(P02.penultimate(List(1,2,2,3,4)) == 3)
+  }
 
+  "given a list with only one number" should "return Nil" in {
+    assert(P02.penultimate(List(1)) == Nil)
+  }
+
+  "given an empty list" should "return Nil" in {
+    assert(P02.penultimate(List()) == Nil)
+  }
+
+  "given a null as parameter" should "return Nil" in {
+    assert(P02.penultimate(null) == Nil)
+  }
+
+  "given list of charactes" should "return the las but one" in {
+    assert(P02.penultimate(List('a','b','b','c','d')) == 'c')
+  }
 }
 
