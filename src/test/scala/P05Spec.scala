3d2158343eb31f147fd18afa5cacb10e2262441e
damian.albrun <damian.albrun@globallogic.com>
2015-01-20 17:19:08 -0300

    Add test cases

--- src/test/scala/P05Spec.scala
+++ src/test/scala/P05Spec.scala
@@ -8,5 +8,17 @@ class P05Spec extends UnitSpec {
     assert(P05.reverse(List(1,2,3)) == List(3,2,1))
   }
 
+  "given an empty list" should "return an empty list as well" in {
+    assert(P05.reverse(List()) == List())
+  }
+
+  "given a null as parameter" should "return Nil" in {
+    assert(P05.reverse(null) == Nil)
+  }
+
+  "given a list with one element" should "return the same list" in {
+    assert(P05.reverse(List(1)) == List(1))
+  }
+
 }
 
