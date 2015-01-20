2aaf0983596f5138748a8e807b348b556551fd7f
damian.albrun <damian.albrun@globallogic.com>
2015-01-20 18:03:57 -0300

    Problem 6 solution: check if is a palindrome list or not

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -1,7 +1,20 @@
 package org.p99.scala
 
 object P06 {
+  def isPalindrome(list: List[Any]): Boolean = {
+    def splitAndCompare(l: List[Any]): Boolean = {
+      for (i <- 0 to list.size / 2) {
+        if (list(i) != list(list.size - 1 - i)) {
+          return false
+        }
+      }
+      return true
+    }
 
-  // TODO
+    list match {
+      case Nil => true
+      case _ => splitAndCompare (list)
+    }
+  }
 
 }
--- src/test/scala/P06Spec.scala
+++ src/test/scala/P06Spec.scala
@@ -1,10 +1,19 @@
 package org.p99.scala
 
-import org.scalatest._
 
 class P06Spec extends UnitSpec {
 
-  // TODO
+  "given a list with elements which his sequence repeats after the 3rd" should "return that is a palindrome" in {
+    assert(P06.isPalindrome(List(1,2,3,2,1)))
+  }
+
+  "given an empty list" should "return that is a palindrome list" in {
+    assert(P06.isPalindrome(List()))
+  }
+
+  "given a list non-palindrome" should "return false" in {
+    assert(!P06.isPalindrome(List(1,2,3)))
+  }
 
 }
 
