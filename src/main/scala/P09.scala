5b61707712441f7ee03e0eace3c566639e3cb40f
insdami <damian.albrun@paddypower.com>
2015-10-29 16:25:46 +0000

    Pack elements

--- src/main/scala/P09.scala
+++ src/main/scala/P09.scala
@@ -1,6 +1,12 @@
 package org.p99.scala
 
 object P09 {
+  def pack(elements: List[Any]) = elements.tail.foldLeft(List(List(elements.head))) { (pack, element) =>
+    pack.last.head match {
+      case `element` => pack.slice(0, pack.indexOf(pack.last)) ++ List(pack.last ++ List(element))
+      case _ => pack ++ List(List(element))
+    }
+  }
 
   // TODO
 
--- src/test/scala/P09Spec.scala
+++ src/test/scala/P09Spec.scala
@@ -1,10 +1,21 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P09Spec extends UnitSpec {
 
-  // TODO
+  "given a list of consecutive duplicate symbols" should "return a list without consecutive duplicates" in {
+    assert(P09.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ==
+      List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+  }
+
+  "given a list of consecutive duplicate strings" should "return a list without consecutive duplicates" in {
+    assert(P09.pack(List("a", "a", "a", "a", "b", "c", "a", "a", "d", "d")) ==
+      List(List("a", "a", "a", "a"), List("b"), List("c"), List("a", "a"), List("d", "d")))
+  }
+
+  "given a list of consecutive duplicate ints" should "return a list without consecutive duplicates" in {
+    assert(P09.pack(List(1,1,1,1,2,2,1,3,3,3,4,4,5)) ==
+      List(List(1,1,1,1), List(2,2), List(1), List(3,3,3), List(4,4), List(5)))
+  }
 
 }
 
