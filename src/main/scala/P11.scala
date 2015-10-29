dba3d81652e13ddf91f9289b04350a0ff86f9861
insdami <damian.albrun@paddypower.com>
2015-10-29 16:50:58 +0000

    run-length encode modified data

--- src/main/scala/P11.scala
+++ src/main/scala/P11.scala
@@ -1,7 +1,13 @@
 package org.p99.scala
 
 object P11 {
+  def encodeModified(elements: List[Any]) = P09.pack(elements).foldLeft(List.empty[Any]) { (list, pack) =>
+    pack match {
+      case x :: Nil => list :+ x
+      case x :: xs => list :+ (pack.size, x)
+    }
+  }
 
-  // TODO
+
 
 }
--- src/test/scala/P11Spec.scala
+++ src/test/scala/P11Spec.scala
@@ -1,10 +1,13 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P11Spec extends UnitSpec {
 
-  // TODO
+  "given a list of consecutive duplicate symbols" should
+    "return a list number of duplicates of the element, leaving as a single element the one non-duplicated" in {
+    assert(P11.encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+      == List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e)))
+  }
+
 
 }
 
