179c7eade8f3960b6e380aa5fd143337aacbae99
insdami <damian.albrun@paddypower.com>
2015-10-29 16:45:54 +0000

    run-length encoding data

--- src/test/scala/P10Spec.scala
+++ src/test/scala/P10Spec.scala
@@ -2,7 +2,7 @@ package org.p99.scala
 
 class P10Spec extends UnitSpec {
 
-  "given a list of consecutive duplicate symbols" should "return a list without consecutive duplicates" in {
+  "given a list of consecutive duplicate symbols" should "return a list number of duplicates of the element" in {
     assert(P10.encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
       == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
   }
