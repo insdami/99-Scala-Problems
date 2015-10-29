56efbced2b3355f85ed07e2a7fba4b7af8cc2fea
insdami <damian.albrun@paddypower.com>
2015-10-29 15:15:34 +0000

    Flatten problem

--- src/main/scala/P07.scala
+++ src/main/scala/P07.scala
@@ -1,7 +1,11 @@
 package org.p99.scala
 
 object P07 {
-
-  // TODO
-
+  def flatten(complexList: List[Any]): List[Any] = complexList.foldLeft(List.empty[Any]) { (flatList, element) =>
+    element match {
+      case Nil => flatList
+      case x :: xs => flatList ++ List(x) ++ flatten(xs)
+      case _ => flatList :+ element
+    }
+  }
 }
--- src/test/scala/P07Spec.scala
+++ src/test/scala/P07Spec.scala
@@ -1,10 +1,31 @@
 package org.p99.scala
 
-import org.scalatest._
 
 class P07Spec extends UnitSpec {
 
-  // TODO
+  "given a list of list" should "return a list with all the involved elements" in {
+    assert(P07.flatten(List(List(1,2), List(3,4), List(5,6))) == List(1,2,3,4,5,6))
+  }
+
+  "given a list of list and seq" should "return a list with all the involved elements" in {
+    assert(P07.flatten(List(List(1,2), Seq(3,4), List(5,6))) == List(1,2,3,4,5,6))
+  }
+
+  "given a list of list, seq and Ints" should "return a list with all the involved elements" in {
+    assert(P07.flatten(List(List(1,2), Seq(3,4), List(5,6), 7, 8)) == List(1,2,3,4,5,6,7,8))
+  }
+
+  "given a list of list, seq, Ints and Strings" should "return a list with all the involved elements" in {
+    assert(P07.flatten(List(List(1,2), Seq(3,4), List(5,6), 7, 8, "9", "10")) == List(1,2,3,4,5,6,7,8,"9","10"))
+  }
+
+  "given a list of list, seq, Ints, Strings and null" should "return a list with all the involved elements" in {
+    assert(P07.flatten(List(List(1,2), Seq(3,4), List(5,6), 7, 8, "9", "10", null)) == List(1,2,3,4,5,6,7,8,"9","10", null))
+  }
+
+  "given a list of empty Lists" should "return an empty List" in {
+    assert(P07.flatten(List(List.empty, List.empty, List.empty)) == List())
+  }
 
 }
 
