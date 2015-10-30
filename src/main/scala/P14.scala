b896e55f56637eee20d7c5da1fc61642a79fe7fb
insdami <damian.albrun@paddypower.com>
2015-10-30 15:57:14 +0000

    Duplicate elements

--- src/main/scala/P14.scala
+++ src/main/scala/P14.scala
@@ -1,7 +1,10 @@
 package org.p99.scala
 
 object P14 {
-
-  // TODO
+  def duplicate(elements: List[Any]) =
+    if(elements == null)
+      List.empty
+    else
+      elements.foldLeft(List.empty[Any])((duplicatedList, element) => duplicatedList ++ List.fill(2)(element))
 
 }
--- src/test/scala/P14Spec.scala
+++ src/test/scala/P14Spec.scala
@@ -1,10 +1,23 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P14Spec extends UnitSpec {
 
-  // TODO
+  "Given a list " should "return a list with duplicated elements" in {
+    assert(P14.duplicate(List('a, 'b, 'c, 'c, 'd))
+      == List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+  }
+
+  "Given an empty list " should "return an empty list" in {
+    assert(P14.duplicate(List.empty) == List.empty)
+  }
+
+  "Given a Nil " should "return an empty list" in {
+    assert(P14.duplicate(Nil) == List.empty)
+  }
+
+  "Given a null " should "return an empty list" in {
+    assert(P14.duplicate(null) == List.empty)
+  }
 
 }
 
