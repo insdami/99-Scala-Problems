84b82a3e78ec80aebe5be44f2224e1f216408a88
insdami <damian.albrun@paddypower.com>
2015-10-30 16:29:23 +0000

    Duplicate n times

--- src/main/scala/P15.scala
+++ src/main/scala/P15.scala
@@ -1,6 +1,13 @@
 package org.p99.scala
 
 object P15 {
+  def duplicateN(times: Int, elements: List[Any]) =
+    if (elements == null)
+      List.empty[Any]
+    else if (times < 1)
+      elements
+    else
+      elements.foldLeft(List.empty[Any])((duplicatedList, element) => duplicatedList ++ List.fill(times)(element))
 
   // TODO
 
--- src/test/scala/P15Spec.scala
+++ src/test/scala/P15Spec.scala
@@ -1,10 +1,26 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P15Spec extends UnitSpec {
 
-  // TODO
+  "Given a list " should "return a list within duplicated n times the elements" in {
+    assert(P15.duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+      == List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd))
+  }
+
+  "Given a list and 1 times to duplicate " should "return a list within non-duplicated elements" in {
+    assert(P15.duplicateN(1, List('a, 'b, 'c, 'c, 'd))
+      == List('a, 'b, 'c, 'c, 'd))
+  }
+
+  "Given a list and 0 times to duplicate " should "return a list within non-duplicated elements" in {
+    assert(P15.duplicateN(0, List('a, 'b, 'c, 'c, 'd))
+      == List('a, 'b, 'c, 'c, 'd))
+  }
+
+  "Given an empty list " should "return an empty list" in {
+    assert(P15.duplicateN(3, List.empty)
+      == List.empty)
+  }
 
 }
 
