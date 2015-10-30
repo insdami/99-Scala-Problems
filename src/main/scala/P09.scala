292fdfa42ee390eb30f31ebea0c52db797b8cf70
insdami <damian.albrun@paddypower.com>
2015-10-30 15:48:54 +0000

    Improved the initial solution

--- src/main/scala/P09.scala
+++ src/main/scala/P09.scala
@@ -2,9 +2,9 @@ package org.p99.scala
 
 object P09 {
   def pack(elements: List[Any]) = elements.tail.foldLeft(List(List(elements.head))) { (pack, element) =>
-    pack.last.head match {
-      case `element` => pack.slice(0, pack.indexOf(pack.last)) ++ List(pack.last :+ element)
-      case _ => pack ++ List(List(element))
+    pack.lastOption match {
+      case Some(t) if t.head == `element` => pack.dropRight(1) ++ List(pack.last :+ element)
+      case _ => pack :+ List(element)
     }
   }
 
