7ec00eb15ffd5c9812de458985c88996d3108d0b
insdami <damian.albrun@paddypower.com>
2015-10-30 11:00:48 +0000

    run-length decode

--- src/main/scala/P12.scala
+++ src/main/scala/P12.scala
@@ -1,7 +1,10 @@
 package org.p99.scala
 
 object P12 {
-
-  // TODO
-
+  def decode(elements: List[Any]) = elements.foldLeft(List.empty[Any]){ (decoded, element) =>
+    element match {
+      case tuple:(Int, Any) => decoded ++ List.fill(tuple._1)(tuple._2)
+      case single:Any => decoded :+ single
+    }
+  }
 }
--- src/test/scala/P12Spec.scala
+++ src/test/scala/P12Spec.scala
@@ -1,10 +1,20 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P12Spec extends UnitSpec {
 
-  // TODO
+  "Given a run-length code symbols list generated " should "return an uncompressed list" in {
+    assert(P12.decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+      == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  }
 
+  "Given a run-length code and single symbols list generated " should "return an uncompressed list" in {
+    assert(P12.decode(List((4, 'a), (1, 'b), 'c, 'a, (1, 'd), (4, 'e), 'b))
+      == List('a, 'a, 'a, 'a, 'b, 'c, 'a, 'd, 'e, 'e, 'e, 'e, 'b))
+  }
+
+  "Given a run-length code and single string list generated " should "return an uncompressed list" in {
+    assert(P12.decode(List((4, "a"), (1, "b"), "c", "a", (1, "d"), (4, "e"), "b"))
+      == List("a", "a", "a", "a", "b", "c", "a", "d", "e", "e", "e", "e", "b"))
+  }
 }
 
