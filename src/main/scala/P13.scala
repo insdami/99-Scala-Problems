63276205070c80deedf25436b8b5ecddfc1cffc7
insdami <damian.albrun@paddypower.com>
2015-10-30 15:43:24 +0000

    encodeDirect

--- src/main/scala/P13.scala
+++ src/main/scala/P13.scala
@@ -1,7 +1,11 @@
 package org.p99.scala
 
 object P13 {
-
-  // TODO
-
+  def encodeDirect(elements: List[Any]) = elements.foldLeft(List.empty[(Int, Any)]) { (encoded, element) =>
+    encoded.lastOption match {
+      case Some(t) if t._2 == `element` => encoded.dropRight(1) :+ (t._1 + 1, element)
+      case Some(t) => encoded :+ (1, element)
+      case None => encoded :+ (1, element)
+    }
+  }
 }
--- src/test/scala/P13Spec.scala
+++ src/test/scala/P13Spec.scala
@@ -1,10 +1,23 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P13Spec extends UnitSpec {
 
-  // TODO
+  "Given a run-length encoding of a list " should "return a run-length encoding data compression list" in {
+    assert(P13.encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+      == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+  }
+
+  "Given an empty List " should "return a empty List" in {
+    assert(P13.encodeDirect(List()) == List())
+  }
+
+
+  "Given a run-length encoding of a list within different kind of elements " should "return a run-length encoding data compression list" in {
+    assert(P13.encodeDirect(
+         List('a, "a", "a", 'a, 'b, 'c, 'c, 'a, 'a, 'd, "e", 'e, 'e, 'e))
+      == List((1,'a), (2, "a"), (1, 'a), (1,'b), (2,'c), (2,'a), (1,'d), (1, "e"), (3,'e)))
+  }
+
 
 }
 
