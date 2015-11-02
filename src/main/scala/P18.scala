433c7a670a5c35df815d02e79bc6e3a54cbd898d
insdami <damian.albrun@paddypower.com>
2015-11-02 14:12:32 +0000

    Slice list

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -1,7 +1,20 @@
 package org.p99.scala
 
 object P18 {
+  def slice(from: Int, to: Int, elements: List[Any]) = {
 
-  // TODO
+    def cut (from: Int, to: Int, original: List[Any], cutList:List[Any] = List.empty ): List[Any] = {
+      if(from == to
+        || original == Nil
+        || original == null
+        || (from > to || from > original.size - 1 || to > original.size - 1)
+        || (from < 0)
+      )
+        cutList
+      else
+        cut(from + 1, to, original, cutList :+ original(from))
+    }
+    cut(from, to, elements)
+  }
 
 }
--- src/test/scala/P18Spec.scala
+++ src/test/scala/P18Spec.scala
@@ -1,10 +1,41 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P18Spec extends UnitSpec {
 
-  // TODO
+  "Given a list and a range" should "return a list within requested elements based on the range" in {
+    assert(P18.slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      == List('d, 'e, 'f, 'g))
+  }
+
+  "Given an empty list and a range" should "return an empty list" in {
+    assert(P18.slice(3, 7, List.empty)
+      == List.empty)
+  }
+
+  "Given an empty list and a range that includes all the indexes" should "return the list except the last" in {
+    assert(P18.slice(0, 2, List('a, 'b, 'c))
+      == List('a, 'b))
+  }
+
+  "Given a null as a list and a range" should "return an empty list" in {
+    assert(P18.slice(3, 7, null)
+      == List.empty)
+  }
+
+  "Given a list and a range over the valid values" should "return an empty list" in {
+    assert(P18.slice(4, 6, List('a, 'b, 'c))
+      == List.empty)
+  }
+
+  "Given a list and a range under the valid values" should "return an empty list" in {
+    assert(P18.slice(-1, 0, List('a, 'b, 'c))
+      == List.empty)
+  }
+
+  "Given a list and a range which has 'from' value bigger than 'to' " should "return an empty list" in {
+    assert(P18.slice(3, 2, List('a, 'b, 'c))
+      == List.empty)
+  }
 
 }
 
