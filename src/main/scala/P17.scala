1434b0f5c5d203423e9edb8bc265e8c0b5405cc4
insdami <damian.albrun@paddypower.com>
2015-11-02 10:32:02 +0000

    Split list

--- src/main/scala/P17.scala
+++ src/main/scala/P17.scala
@@ -1,7 +1,10 @@
 package org.p99.scala
 
 object P17 {
-
-  // TODO
-
+  def split(length: Int, elements: List[Any]) = elements.foldLeft((List.empty[Any], List.empty[Any])) { (result, element) =>
+    if(elements.indexOf(element) < length)
+      (result._1 :+ element, result._2)
+    else
+      (result._1, result._2 :+ element)
+  }
 }
--- src/test/scala/P17Spec.scala
+++ src/test/scala/P17Spec.scala
@@ -1,10 +1,20 @@
 package org.p99.scala
 
-import org.scalatest._
-
 class P17Spec extends UnitSpec {
 
-  // TODO
+  "Given a list and a length" should "return a splitted list where the first list has the requested length" in {
+    assert(P17.split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      == (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+  }
+
+  "Given a list and zero length value" should "return a tuple where the first is an empty list, leaving the original list as a second" in {
+    assert(P17.split(0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      == (List.empty,List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+  }
+
+  "Given an empty list and a length value" should "return a tuple with empty lists" in {
+    assert(P17.split(3, List.empty) == (List.empty,List.empty))
+  }
 
 }
 
